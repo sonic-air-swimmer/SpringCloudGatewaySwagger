b7bcbab1d3b0332e5aeb5fbeee8668711eadc7d0
dantw <D@n@1133557799@2>
2022-02-28 02:19:14 +0800

    Simple Gateway

--- ServiceA/src/main/java/com/servicea/ServiceAController.java
+++ ServiceA/src/main/java/com/servicea/ServiceAController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceAController {
 
-    @GetMapping("servicea/test")
+    @GetMapping("test")
     public String testServiceA() {
         return "Testing ServiceA";
     }
