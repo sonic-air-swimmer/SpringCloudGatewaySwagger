1242e5ad233777d3ad16883d7938e13c8e1b48a9
dantw <D@n@1133557799@2>
2022-04-12 22:23:42 +0800

    Strip prefix and using http headers

--- ServiceA/src/main/java/com/servicea/ServiceAMain.java
+++ ServiceA/src/main/java/com/servicea/ServiceAMain.java
@@ -4,9 +4,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
-import java.util.Collections;
+import javax.servlet.DispatcherType;
+import java.util.List;
 
 @SpringBootApplication
 public class ServiceAMain {
@@ -20,6 +24,16 @@ public class ServiceAMain {
         return new OpenAPI()
                 .info(new Info().title("ServiceA API")
                         .description("Documentation for ServiceA"));
+    }
+
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registration.setUrlPatterns(List.of("/**"));
+        return registration;
     }
 
 }
