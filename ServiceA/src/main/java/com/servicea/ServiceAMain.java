7f10489d511a9abb4739f02fc7231e7eeacd1357
dantw <tw.dan@dhl.com>
2022-04-24 02:43:49 +0800

    Optimize import

--- ServiceA/src/main/java/com/servicea/ServiceAMain.java
+++ ServiceA/src/main/java/com/servicea/ServiceAMain.java
@@ -4,15 +4,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import org.springframework.core.Ordered;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
-import javax.servlet.DispatcherType;
-import java.util.List;
 
 @SpringBootApplication
 public class ServiceAMain {
